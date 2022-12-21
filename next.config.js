2842e63b21beae706d06cf891208f972cb1c39d6
Taras Petrukhno <forstuft@gmail.com>
2022-12-21 15:55:02 +0200

    external photos 1.0

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['picsum.photos'],
-  }
+    loader: "imgix",
+    path: ""
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH //the same as basePath, but for static resource like css javascript
 }
 
 module.exports = nextConfig
