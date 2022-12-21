497349b037c00c86105cfe6b29b334f4575c6904
Taras Petrukhno <forstuft@gmail.com>
2022-12-21 15:48:09 +0200

    external photos

--- next.config.js
+++ next.config.js
@@ -3,12 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['https://picsum.photos'],
-    loader: "akamai",
-    path: ""
-  },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH //the same as basePath, but for static resource like css javascript
+    domains: ['picsum.photos'],
+  }
 }
 
 module.exports = nextConfig
